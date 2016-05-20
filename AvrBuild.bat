ade3954526920036d5e3b7656032b57c2721d015
Glenn McGuire <glennmcguire9@gmail.com>
2016-05-20 13:05:16 +1000

    added gitignore and created AVR studio project

---
+++ AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "Z:\home\glenn\Desktop\COMP2121-16s1\labels.tmp" -fI -W+ie -o "Z:\home\glenn\Desktop\COMP2121-16s1\Ass1.hex" -d "Z:\home\glenn\Desktop\COMP2121-16s1\Ass1.obj" -e "Z:\home\glenn\Desktop\COMP2121-16s1\Ass1.eep" -m "Z:\home\glenn\Desktop\COMP2121-16s1\Ass1.map" "Z:\home\glenn\Desktop\COMP2121-16s1\main.asm"
